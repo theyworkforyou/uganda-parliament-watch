9bf0cdba1a5afe187dbcab06ce3627e8bfe66f6f
Chris Mytton <chrismytton@gmail.com>
2016-04-08 18:52:36 +0200

    Output multiple current MPs for a constituency
    This is needed temporarily while we treat the Women's Representatives as
    a constituency.

--- _plugins/constituency_mps.rb
+++ _plugins/constituency_mps.rb
@@ -6,7 +6,7 @@ module Jekyll
       most_recent_term = site.collections['events'].docs.find { |d| d.data['end_date'].nil? }
       site.collections['areas'].docs.each do |area|
         current, historic = area.data['memberships'].partition { |m| m['legislative_period'] == most_recent_term && m['end_date'].nil? }
-        area.data['current_memberships'] = current.first
+        area.data['current_memberships'] = current
         area.data['historic_memberships'] = historic
       end
     end
