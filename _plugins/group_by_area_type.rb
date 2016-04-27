0449c65968c4668c01c696580a387d5604f51e67
Chris Mytton <chrismytton@gmail.com>
2016-04-27 11:03:59 +0200

    Make it clearer that some memberships are being dropped

--- _plugins/group_by_area_type.rb
+++ _plugins/group_by_area_type.rb
@@ -3,8 +3,11 @@ class GroupByAreaType < Jekyll::Generator
     ocd_ids = CSV.parse(open('https://github.com/theyworkforyou/uganda_ocd_ids/raw/master/identifiers/country-ug.csv').read, headers: true, header_converters: :symbol)
     ocd_mapping = Hash[ocd_ids.map { |id| [id[:id], id[:name]] }]
     # Group current memberships by district
-    memberships_by_district = site.collections['memberships'].docs.reject { |d| !d['area_id'] }.group_by do |d|
-      d['area_id'] && d['area_id'].split('/').slice_after(/^district\:/).first.join('/')
+    memberships, memberships_without_area = site.collections['memberships'].docs.partition do |membership|
+      membership['area_id']
+    end
+    memberships_by_district = memberships.group_by do |d|
+      d['area_id'].split('/').slice_after(/^district\:/).first.join('/')
     end
     # Create a new collection
     collection_name = 'districts'
