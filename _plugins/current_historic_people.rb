33aece739c26baae8b8862df73715b1723c3a9d2
Chris Mytton <chrismytton@gmail.com>
2016-05-05 12:31:39 +0200

    Update for latest jekyll-popolo API

--- _plugins/current_historic_people.rb
+++ _plugins/current_historic_people.rb
@@ -6,9 +6,9 @@ module Jekyll
     def generate(site)
       most_recent_term = site.collections['events'].docs.find { |d| d.data['end_date'].nil? }
       site.collections['people'].docs.each do |person|
-        if person.data['memberships'].any? { |m| m['legislative_period'] == most_recent_term }
+        if person.data['memberships'].any? { |m| m['legislative_period_id'] == most_recent_term['id'] }
           person.data['layout'] = CURRENT_MP_LAYOUT
-          person.data['current_membership'] = person.data['memberships'].find { |m| m['legislative_period'] == most_recent_term }
+          person.data['current_membership'] = person.data['memberships'].find { |m| m['legislative_period_id'] == most_recent_term['id'] }
         else
           person.data['layout'] = HISTORIC_MP_LAYOUT
         end
--- _plugins/popolo.rb
+++ _plugins/popolo.rb
@@ -1,21 +1,40 @@
 require 'open-uri'
 
-Jekyll::Popolo.register(:parliament, open(File.read('EVERYPOLITICIAN_DATASOURCE').chomp).read)
+Jekyll::Popolo.register_popolo_file(:parliament, open(File.read('EVERYPOLITICIAN_DATASOURCE').chomp).read)
 
-Jekyll::Popolo.process(:parliament) do |site, popolo|
-  popolo['memberships'].each do |membership|
-    membership['person'] = popolo['persons'].find { |p| p['id'] == membership['person_id'] }
-    membership['party'] = popolo['organizations'].find { |o| o['id'] == membership['on_behalf_of_id'] }
-    membership['term'] = popolo['events'].find { |e| e['id'] == membership['legislative_period_id'] }
-    membership['post'] = popolo['posts'].find { |p| p['id'] == membership['post_id'] }
-    membership['area'] = popolo['areas'].find { |a| a['id'] == membership['area_id'] }
+Jekyll::Popolo.process do |site, popolo|
+  parliament = popolo.files[:parliament]
+  people = parliament['persons'].map do |person|
+    person['layout'] = 'people'
+    person['title'] = person['name']
+    if person['image']
+      person['image'] = "https://theyworkforyou.github.io/uganda-images/Parliament/#{person['id']}.jpeg"
+    end
+    person
   end
-  posts = popolo['posts'].map do |p|
+  popolo.create_jekyll_collections(people: people)
+
+  memberships = parliament['memberships'].map do |membership|
+    membership['person'] = site.collections['people'].docs.find { |p| p['id'] == membership['person_id'] }
+    membership['party'] = parliament['organizations'].find { |o| o['id'] == membership['on_behalf_of_id'] }
+    membership['legislative_period'] = parliament['events'].find { |e| e['id'] == membership['legislative_period_id'] }
+    membership['post'] = parliament['posts'].find { |p| p['id'] == membership['post_id'] }
+    membership['area'] = parliament['areas'].find { |a| a['id'] == membership['area_id'] }
+    membership
+  end
+  popolo.create_jekyll_collections(memberships: memberships)
+
+  # Associate people with their memberships
+  site.collections['people'].docs.each do |person|
+    person.data['memberships'] = site.collections['memberships'].docs.find_all { |m| m['person_id'] == person['id'] }
+  end
+
+  posts = parliament['posts'].map do |p|
     layout = Jekyll::Utils.slugify(p['label']).gsub('-', '_')
     if site.layouts.key?(layout)
       p['layout'] = layout
     else
-      Jekyll.logger.warn "Couldn't find layout: #{layout} for post #{p}"
+      Jekyll.logger.warn "Couldn't find layout: #{layout} for post #{p['id']}"
       p['layout'] = 'default'
     end
     p['title'] = p['label']
@@ -23,20 +42,20 @@ Jekyll::Popolo.process(:parliament) do |site, popolo|
   end
 
   term_9_posts = posts.map do |p|
-    p['memberships'] = popolo['memberships'].find_all do |m|
+    p['memberships'] = parliament['memberships'].find_all do |m|
       m['post_id'] == p['id'] && m['legislative_period_id'] == 'term/9'
     end
     p
   end
   term_10_posts = posts.map do |p|
-    p['memberships'] = popolo['memberships'].find_all do |m|
+    p['memberships'] = parliament['memberships'].find_all do |m|
       m['post_id'] == p['id'] && m['legislative_period_id'] == 'term/10'
     end
     p
   end
 
-  {
+  popolo.create_jekyll_collections(
     term_9_posts: term_9_posts,
     term_10_posts: term_10_posts,
-  }
+  )
 end
